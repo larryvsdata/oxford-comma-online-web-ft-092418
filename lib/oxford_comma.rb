3dce4ae96ea9770626f3e5efe20358c15beaf37f
larryvsdata <ermanbekaroglu@gmail.com>
2018-09-27 18:08:44 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
-my_string=""
-for index in 0 ... array.size-1
-  my_string=my_string<<", "<<array[index]
+
+if array.size >1
+  array[-1]
+else
+  array[0]
 end
+  
 
-my_string=my_string<<", and "<<array[array.size-1]
-
-return my_string
 end
