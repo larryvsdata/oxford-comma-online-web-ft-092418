63659b075fa1edfbe84060d81da154df09e768b9
larryvsdata <ermanbekaroglu@gmail.com>
2018-09-27 18:14:56 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,11 @@ def oxford_comma(array)
 
 if array.size >2
   array[-1]="and "<<array[-1]
+  return array.join(", ")
 elsif array.size ==2
-  array[0]
+ return  array[0]<<" and "<<array[1]
+else
+return array[0]
 end
   
 
