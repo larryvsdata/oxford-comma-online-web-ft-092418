7f00d07b52a7397d2f65d42f564c76ad41bb6212
larryvsdata <ermanbekaroglu@gmail.com>
2018-09-27 18:05:51 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,7 @@ for index in 0 ... array.size-1
   my_string=my_string<<", "<<array[index]
 end
 
-my_string=my_string<<", "<<array[array.size-1]
+my_string=my_string<<", and "<<array[array.size-1]
 
+return my_string
 end
